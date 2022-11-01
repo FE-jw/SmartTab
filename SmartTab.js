60e3985605550597a66efe0682475fc47fc63957
zaixu <zaixu91@gmail.com>
2022-11-01 22:41:19 +0900

    ver 1 개발중

--- SmartTab.js
+++ SmartTab.js
@@ -1,13 +1,26 @@
 class SmartTab{
 	constructor(options){
-		const btnWrap = document.querySelector(options.btnEle);
-		const btnLists = Array.prototype.slice.call(btnWrap.querySelectorAll('li'));
-		const btns = Array.prototype.slice.call(btnWrap.querySelectorAll('a'));
-		const contents = Array.prototype.slice.call(document.querySelectorAll(options.contentEle));
-		let currentActiveIndex = typeof options.firstTabIndex !== 'undefined' ? options.firstTabIndex : 0;
+		this.options = options;
+		this.btnWrap = document.querySelector(options.btnEle);
+		this.btnLists = Array.prototype.slice.call(this.btnWrap.querySelectorAll('li'));
+		this.btns = Array.prototype.slice.call(this.btnWrap.querySelectorAll('a'));
+		this.contents = Array.prototype.slice.call(document.querySelectorAll(options.contentEle));
+		this.currentActiveIndex = typeof options.firstTabIndex !== 'undefined' ? options.firstTabIndex : 0;
+
+		this.init();
+		this.changeTab();
+	}
+
+	init(){
+		let options = this.options;
+		let btnWrap = this.btnWrap;
+		let btnLists = this.btnLists;
+		let btns = this.btns;
+		let contents = this.contents;
+		let currentActiveIndex = this.currentActiveIndex;
 
 		btnWrap.role = 'tablist';
-	
+		
 		btnLists.forEach((ele, index) => {
 			ele.role = 'none'
 			ele.querySelector('a').dataset.index = index;
@@ -17,44 +30,76 @@ class SmartTab{
 			ele.role = 'tab';
 			ele.ariaSelected = false;
 			ele.setAttribute('aria-controls', ele.hash.replace('#', ''));
+		});
+	
+		contents.forEach(ele => {
+			ele.role = 'tabpanel';
+			ele.tabIndex = 0;
+	
+			if(!options.cssClassMode){
+				ele.style.display = 'none';
+			}
+	
+			if(!options.tabOutline){
+				ele.style.outline = 'none';
+			}
+		});
+	
+		//First Active Tab
+		let initTab = document.querySelector('a[data-index="' + currentActiveIndex + '"]');
+		initTab.classList.add('active');
+		initTab.ariaSelected = true;
+	
+		if(!options.cssClassMode){
+			document.getElementById(initTab.getAttribute('aria-controls')).style.display = '';
+		}else{
+			document.getElementById(initTab.getAttribute('aria-controls')).classList.add(options.cssClassMode);
+		}
+	}
 
-			//Click on the A tag
+	changeTab(callback){
+		let options = this.options;
+		let btnWrap = this.btnWrap;
+		let btns = this.btns;
+		let currentActiveIndex = this.currentActiveIndex;
+		
+		btns.forEach(ele => {
 			ele.addEventListener('click', function(e){
 				e.preventDefault();
-
-				let selected = this.ariaSelected;
-				let currentIndex = this.dataset.index;
+	
 				let beforeTab;
 				let beforeIndex;
+				let selected = this.ariaSelected;
+				let currentIndex = this.dataset.index;
 				let currentTabHash = this.getAttribute('aria-controls');
 	
 				if(selected == 'false'){
+					//Not the first click
 					if(btnWrap.querySelector('a[aria-selected=true]')){
-						//Not the first click
 						let _this = btnWrap.querySelector('a[aria-selected=true]');
 						beforeIndex = _this.dataset.index;
 						beforeTab = _this.getAttribute('aria-controls');
-
+	
 						_this.ariaSelected = false;
 						_this.classList.remove('active');
-
+	
 						if(!options.cssClassMode){
 							document.getElementById(beforeTab).style.display = 'none';
 						}else{
 							document.getElementById(beforeTab).classList.remove(options.cssClassMode);
 						}
 					}
-
+	
 					this.ariaSelected = true;
 					this.classList.add('active');
-
+	
 					if(!options.cssClassMode){
 						console.log(document.getElementById(currentTabHash));
 						document.getElementById(currentTabHash).style.display = '';
 					}else{
 						document.getElementById(currentTabHash).classList.add(options.cssClassMode);
 					}
-
+	
 					currentActiveIndex = currentIndex;
 				}
 	
@@ -62,31 +107,10 @@ class SmartTab{
 				document.getElementById(currentTabHash).style.display = '';
 				document.getElementById(currentTabHash).focus();
 				window.scrollTo(0, winTop);
+
+				//Callback
+				callback && callback();
 			});
 		});
-
-		contents.forEach(ele => {
-			ele.role = 'tabpanel';
-			ele.tabIndex = 0;
-
-			if(!options.cssClassMode){
-				ele.style.display = 'none';
-			}
-
-			if(!options.tabOutline){
-				ele.style.outline = 'none';
-			}
-		});
-
-		//Init
-		let initTab = document.querySelector('a[data-index="' + currentActiveIndex + '"]');
-		initTab.classList.add('active');
-		initTab.ariaSelected = true;
-
-		if(!options.cssClassMode){
-			document.getElementById(initTab.getAttribute('aria-controls')).style.display = '';
-		}else{
-			document.getElementById(initTab.getAttribute('aria-controls')).classList.add(options.cssClassMode);
-		}
 	}
 }
