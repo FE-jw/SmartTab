4543dd66ca1e872a8321a399d8e4c3d7f37c42d5
zaixu <zaixu91@gmail.com>
2022-11-14 23:24:02 +0900

    version 1.1.0 개발중

--- SmartTab.js
+++ SmartTab.js
@@ -1,8 +1,8 @@
 /**
- * Version: 1.0.1
+ * Version: 1.1.0
  * Web: https://fe-jw.github.io/SmartTab
  * GitHub: https://github.com/FE-jw/SmartTab
- * Released: 2022-11-09
+ * Released: 2022-##-##
 */
 
 class SmartTab{
@@ -14,53 +14,63 @@ class SmartTab{
 		this.contents = Array.prototype.slice.call(document.querySelectorAll(options.contentEle));
 		this.currentActiveIndex = typeof options.firstTabIndex !== 'undefined' ? options.firstTabIndex : 0;
 
+		this.initialize();
+	}
+
+	a11y(){
 		//A11Y
 		this.btnWrap.role = 'tablist';
+
 		this.btnLists.forEach((li, index) => {
 			li.role = 'none'
 			li.querySelector('a').dataset.index = index;
 		});
+
 		this.btns.forEach(btn => {
 			btn.role = 'tab';
 			btn.ariaSelected = false;
 			btn.setAttribute('aria-controls', btn.hash.replace('#', ''));
 		});
+		
 		this.contents.forEach(content => {
 			content.role = 'tabpanel';
 			content.tabIndex = 0;
 	
-			if(!options.tabOutline){
+			if(!this.options.tabOutline){
 				content.style.outline = 'none';
 			}
 		});
-	
+	}
+
+	initialize(){
+		this.a11y();
+
 		//Initialize(First Active Tab)
 		let initTab = this.btnWrap.querySelector('a[data-index="' + this.currentActiveIndex + '"]');
-		initTab.classList.add(options.cssModeClass);
+		initTab.classList.add(this.options.cssModeClass);
 		initTab.ariaSelected = true;
-		document.getElementById(initTab.getAttribute('aria-controls')).classList.add(options.cssModeClass);
+		document.getElementById(initTab.getAttribute('aria-controls')).classList.add(this.options.cssModeClass);
 
 		//Click Tab Button
-		let btnWrap = this.btnWrap;
 		this.btns.forEach(ele => {
-			ele.addEventListener('click', function(e){
+			ele.addEventListener('click', (e) => {
 				e.preventDefault();
-	
-				if(this.ariaSelected == 'false'){
+
+				if(ele.ariaSelected == 'false'){
 					//Not the first click
-					let _this = btnWrap.querySelector('a[aria-selected=true]');
+					let _this = this.btnWrap.querySelector('a[aria-selected=true]');
 					let beforeTab = _this.getAttribute('aria-controls');
 
 					_this.ariaSelected = false;
-					_this.classList.remove(options.cssModeClass);
-					document.getElementById(beforeTab).classList.remove(options.cssModeClass);
+					_this.classList.remove(this.options.cssModeClass);
+					document.getElementById(beforeTab).classList.remove(this.options.cssModeClass);
 	
-					this.ariaSelected = true;
-					this.classList.add(options.cssModeClass);
+					ele.ariaSelected = true;
+					ele.classList.add(this.options.cssModeClass);
 	
 					let winTop = window.scrollY;
-					let currentTabHash = this.getAttribute('aria-controls');
-					document.getElementById(currentTabHash).classList.add(options.cssModeClass);
+					let currentTabHash = ele.getAttribute('aria-controls');
+					document.getElementById(currentTabHash).classList.add(this.options.cssModeClass);
 					document.getElementById(currentTabHash).focus();
 					window.scrollTo(0, winTop);
 				}
@@ -69,6 +79,8 @@ class SmartTab{
 	}
 
 	changeTab(callback){
+		console.log('changeTab');
+		/*
 		this.btns.forEach(ele => {
 			ele.addEventListener('click', function(){
 				callback && callback(
@@ -78,5 +90,6 @@ class SmartTab{
 				);
 			});
 		});
+		*/
 	}
 }
